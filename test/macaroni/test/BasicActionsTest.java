e9222b29a6d9443a764e977000e8567fa2c18d3c
tomecz-tamas <100798401+tomecz-tamas@users.noreply.github.com>
2024-05-17 20:24:21 +0200

    Test for invalid positions, test for pierce action

--- test/macaroni/test/BasicActionsTest.java
+++ test/macaroni/test/BasicActionsTest.java
@@ -1,32 +1,31 @@
 package macaroni.test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import macaroni.model.character.Plumber;
-import macaroni.model.character.Saboteur;
 import macaroni.model.element.Pipe;
+import macaroni.model.element.Pump;
 import macaroni.model.misc.WaterCollector;
 
 public class BasicActionsTest {
-	private Plumber plumber;
-	private Saboteur saboteur;
 	private Pipe pipe;
+	private Pump pump;
 	private WaterCollector wc;
 
 	@BeforeEach
 	void setup() {
 		wc = new WaterCollector();
 		pipe = new Pipe(wc);
-		plumber = new Plumber(pipe);
-		saboteur = new Saboteur(pipe);
+		pump = new Pump();
 	}
 
 	@Test
 	void repairPipe() {
+		var plumber = new Plumber(pipe);
+
 		pipe.pierce();
 		boolean pierceSuccess = pipe.isPierced();
 		boolean repairSuccess = plumber.repair(pipe);
@@ -34,6 +33,42 @@ public class BasicActionsTest {
 
 		assertTrue(pierceSuccess);
 		assertTrue(repairSuccess);
-		assumeFalse(isPierced);
+		assertFalse(isPierced);
+	}
+
+	@Test
+	void repairPipe_InvalidPos() {
+		var plumber = new Plumber(pump);
+
+		pipe.pierce();
+		boolean pierceSuccess = pipe.isPierced();
+		boolean repairSuccess = plumber.repair(pipe);
+		boolean isPierced = pipe.isPierced();
+
+		assertTrue(pierceSuccess);
+		assertFalse(repairSuccess);
+		assertTrue(isPierced);
+	}
+
+	@Test
+	void piercePipe() {
+		var plumber = new Plumber(pipe);
+
+		boolean pierceSuccess = plumber.pierce(pipe);
+		boolean isPierced = pipe.isPierced();
+
+		assertTrue(pierceSuccess);
+		assertTrue(isPierced);
+	}
+
+	@Test
+	void piercePipe_InvalidPos() {
+		var plumber = new Plumber(pump);
+
+		boolean pierceSuccess = plumber.pierce(pipe);
+		boolean isPierced = pipe.isPierced();
+
+		assertFalse(pierceSuccess);
+		assertFalse(isPierced);
 	}
 }
