935fbc295d0e7cb0a49fd7d8e69fdc4a71ea62bb
BBalint1997 <barcsbalint@gmail.com>
2024-05-18 15:59:18 +0200

    Saboteur 100 coverage Unit tests

---
+++ test/SaboteurTester.java
@@ -0,0 +1,46 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import macaroni.model.element.Pump;
+import macaroni.model.element.Pipe;
+import macaroni.model.character.Saboteur;
+
+
+import static org.mockito.Mockito.*;
+
+public class SaboteurTester {
+    private Pump mockPump;
+    private Pipe mockPipe;
+    private Saboteur testSaboteurOnPipe;
+    private Saboteur testSaboteurOnPump;
+
+    @BeforeEach
+    public void init(){
+        mockPump = mock(Pump.class);
+        mockPipe = mock(Pipe.class);
+        
+        testSaboteurOnPump = new Saboteur(mockPump);
+        testSaboteurOnPipe = new Saboteur(mockPipe);
+    }
+
+    @Test
+    public void dropBananaSuccess(){
+
+        //Act
+        boolean result = testSaboteurOnPipe.dropBanana(mockPipe);
+        assertEquals(result, true);
+
+    }
+
+    @Test
+    public void dropBananaFail(){
+
+        //Act
+        boolean result = testSaboteurOnPump.dropBanana(mockPipe);
+        assertEquals(result, false);
+
+    }
+
+
+}
