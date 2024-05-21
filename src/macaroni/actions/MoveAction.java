b84b01b5c0d7be64d988ec6b8ecc94e6de4ec993
kereszthury <kereszthurypeter@gmail.com>
2024-05-21 16:35:00 +0200

    Fixed effect positioning

--- src/macaroni/actions/MoveAction.java
+++ src/macaroni/actions/MoveAction.java
@@ -3,9 +3,14 @@ package macaroni.actions;
 import macaroni.app.gameView.ViewRepository;
 import macaroni.model.character.Character;
 import macaroni.model.character.Plumber;
+import macaroni.model.effects.NoEffect;
 import macaroni.model.element.Element;
+import macaroni.model.element.Pipe;
+import macaroni.model.element.Pump;
 import macaroni.views.CharacterView;
 import macaroni.views.PipeView;
+import macaroni.views.Position;
+import macaroni.views.PumpView;
 
 /**
  * Mozgási interakciót kezelő osztály.
@@ -41,12 +46,25 @@ public class MoveAction extends Action {
             var characterView = (CharacterView) ViewRepository.getViewOfObject(actor);
             characterView.setPosition(ViewRepository.getViewOfObject(element).getPosition());
             if (actor instanceof Plumber plumber) {
-                if (plumber.getHeldPipe() != null) {
-                    var heldPipeView = (PipeView) ViewRepository.getViewOfObject(plumber.getHeldPipe());
-                    heldPipeView.replaceEndpointPos(
-                            ViewRepository.getViewOfObject(locationBeforeMove).getPosition(),
-                            ViewRepository.getViewOfObject(actor.getLocation()).getPosition()
-                    );
+                var heldPipe = plumber.getHeldPipe();
+                if (heldPipe != null) {
+                    var heldPipeView = (PipeView) ViewRepository.getViewOfObject(heldPipe);
+                    var endpoint1 = ViewRepository.getViewOfObject(locationBeforeMove).getPosition();
+                    var endpoint2 = ViewRepository.getViewOfObject(actor.getLocation()).getPosition();
+
+                    heldPipeView.replaceEndpointPos(endpoint1, endpoint2);
+
+                    var effect = heldPipe.getEffect();
+                    if (!(effect instanceof NoEffect)) {
+                        var v = ViewRepository.getViewOfObject(effect);
+                        v.setPosition(heldPipeView.getPosition());
+                    }
+
+                    var connectedElement = heldPipe.getEndpoint(0) != null ? heldPipe.getEndpoint(0) : heldPipe.getEndpoint(1);
+                    if (connectedElement instanceof Pump) {
+                        var pumpView = (PumpView) ViewRepository.getViewOfObject(connectedElement);
+                        //pumpView.setInputPipePos(); TODO
+                    }
                 }
             }
         }
