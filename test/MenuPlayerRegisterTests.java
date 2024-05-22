8c4c8937906f43b7891aac0a1188ebe944d93a67
homok43 <arminszilczer@gmail.com>
2024-05-22 03:04:11 +0200

    Enter key check in player registering

--- test/MenuPlayerRegisterTests.java
+++ test/MenuPlayerRegisterTests.java
@@ -1,8 +1,10 @@
 import macaroni.app.menuView.*;
+import org.assertj.swing.core.KeyPressInfo;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static java.awt.event.KeyEvent.VK_ENTER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MenuPlayerRegisterTests extends UiTest {
@@ -16,12 +18,18 @@ public class MenuPlayerRegisterTests extends UiTest {
         var textBox = plumberPanel.textBox(matchClass(TeamTextBox.class));
         var submitButton = plumberPanel.button(matchClass(MenuTeamButton.class));
 
-        var plumberName = "slububu";
+        var plumberName1 = "dibu";
         elements.requireItemCount(0);
-        textBox.enterText(plumberName);
+        textBox.enterText(plumberName1);
         submitButton.click();
         elements.requireItemCount(1);
-        assertEquals(plumberName, elements.contents()[0]);
+        assertEquals(plumberName1, elements.contents()[0]);
+
+        var plumberName2 = "mibu";
+        elements.requireItemCount(1);
+        textBox.enterText(plumberName2).pressAndReleaseKey(KeyPressInfo.keyCode(VK_ENTER));
+        elements.requireItemCount(2);
+        assertEquals(plumberName2, elements.contents()[1]);
     }
 
     @Test
@@ -34,11 +42,17 @@ public class MenuPlayerRegisterTests extends UiTest {
         var textBox = saboteurPanel.textBox(matchClass(TeamTextBox.class));
         var submitButton = saboteurPanel.button(matchClass(MenuTeamButton.class));
 
-        var saboteurName = "wabalaba";
+        var saboteurName1 = "subulubu";
         elements.requireItemCount(0);
-        textBox.enterText(saboteurName);
+        textBox.enterText(saboteurName1);
         submitButton.click();
         elements.requireItemCount(1);
-        assertEquals(saboteurName, elements.contents()[0]);
+        assertEquals(saboteurName1, elements.contents()[0]);
+
+        var saboteurName2 = "wabalaba";
+        elements.requireItemCount(1);
+        textBox.enterText(saboteurName2).pressAndReleaseKey(KeyPressInfo.keyCode(VK_ENTER));
+        elements.requireItemCount(2);
+        assertEquals(saboteurName2, elements.contents()[1]);
     }
 }
